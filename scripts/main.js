3cfafb02f72c024d4ed2194d5c19a71729d4daa3
Alexey Merkulov <bgates@inbox.ru>
2019-07-06 13:21:28 +0300

    Добавлен слайдер brands

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    //initialize swiper when document ready
+    
+  
     var mySwiper = new Swiper ('.swiper-container', {
       // Optional parameters
         direction: 'horizontal',
@@ -13,5 +14,18 @@ $(document).ready(function () {
             clickable: true,
             type: 'bullets'
         }
+    })
+
+    var mySwiper1 = new Swiper ('.swiper-container-brands', {
+      // Optional parameters
+      slidesPerView: 5,
+      spaceBetween: 0,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
     })
   });
